6f8642bda77ef9746781d6020e7ae0abcaabea02
Anonymous <none>
2015-08-07 13:27:48 -0700

    update version 15

--- randomizer.py
+++ randomizer.py
@@ -17,7 +17,7 @@ from uniso import remove_sector_metadata, inject_logical_sectors
 def randint(a, b):
     return random.randint(min(a, b), max(a, b))
 
-VERSION = "14"
+VERSION = "15"
 MD5HASHES = ["aefdf27f1cd541ad46b5df794f635f50",
              "b156ba386436d20fd5ed8d37bab6b624",
              ]
