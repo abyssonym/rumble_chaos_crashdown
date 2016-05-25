e0a98c7d82a153a94655441d6803f4b50ccc2c03
Anonymous <none>
2016-05-24 21:10:40 -0700

    update version 21 DELITA

--- randomizer.py
+++ randomizer.py
@@ -61,7 +61,7 @@ def check_progress_counter(index):
     stdout.flush()
 
 
-VERSION = "19"
+VERSION = "21"
 MD5HASHES = ["b156ba386436d20fd5ed8d37bab6b624",
              "aefdf27f1cd541ad46b5df794f635f50",
              ]
