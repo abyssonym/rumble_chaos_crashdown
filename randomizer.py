6780e2c61dfac2576b8f6341d5a120d195e6a41b
Anonymous <none>
2016-07-24 11:40:05 -0700

    update version 22 ALMA

--- randomizer.py
+++ randomizer.py
@@ -62,7 +62,7 @@ def check_progress_counter(index):
     stdout.flush()
 
 
-VERSION = "21"
+VERSION = "22"
 MD5HASHES = ["b156ba386436d20fd5ed8d37bab6b624",
              "aefdf27f1cd541ad46b5df794f635f50",
              ]
