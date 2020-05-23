e77d42050fddd8dc90df0f9a4710413fcb16a87b
Anonymous <none>
2020-05-23 08:30:42 -0700

    update version 27

--- randomizer.py
+++ randomizer.py
@@ -152,7 +152,7 @@ def check_progress_counter(index):
     stdout.flush()
 
 
-VERSION = "26"
+VERSION = "27"
 MD5HASHES = ["b156ba386436d20fd5ed8d37bab6b624",
              "aefdf27f1cd541ad46b5df794f635f50",
              ]
