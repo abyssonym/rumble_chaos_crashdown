7aafef34c9f7993c8175c8421735d91bcd1d6a6b
George Georgulas <georgegeorgulas@gmail.com>
2020-10-22 03:20:26 -0400

    Update docker-entrypoint.sh
    Added spoiler export option

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # randomize
-python /rumble_chaos_crashdown/randomizer.py /input/$filename
+echo "Running randomizer python script with command:"
+echo "'python /rumble_chaos_crashdown/randomizer.py /input/${FILENAME} ${FLAGS}${SEED} ${CHAOS}'"
+python /rumble_chaos_crashdown/randomizer.py /input/${FILENAME} ${FLAGS}${SEED} ${CHAOS}
 
 # rename .iso to .bin for OpenEmu
  for file in *.iso
@@ -15,3 +17,8 @@ python /rumble_chaos_crashdown/randomizer.py /input/$filename
  # change file extension and close quotes
   sed -i '1 s/iso/bin"/' ${file%.iso}.cue
 mv ${file%.iso}.cue /output
+
+# export spoiler log from container
+if [ $EXPORTSPOILER ] ; then
+  mv *.txt /output
+fi
